76d84cb6b2ad2190a619698b83caa4fe9e630985
Gabriel Flores <g.flores@correo.unimet.edu.ve>
2024-05-23 21:53:27 -0400

    commit javascript

---
+++ scripts/list.js
@@ -0,0 +1,14 @@
+const wordlist = [
+  {
+    word:"guitar",
+    hint:"A musical instrument with strings."
+  },
+  {
+    word:"oxygen",
+    hint:"A colorless, odorless gas essential for life."
+  },
+  {
+    word:"mountain",
+    hint:"A large natural elevation of the Earth`s surface."
+  }
+];
